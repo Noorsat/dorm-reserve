abc3c4e5a463f8ee168535582b16817712a9e858
Nursat Bakyt <nursat@MacBook-Pro-Nursat.local>
2023-04-13 15:15:26 +0600

    f

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,9 +1,102 @@
-import { FC } from 'react'
-import styles from './Header.module.css'
+"use client"; 
+
+import { FC, useState } from 'react'
+import styles from './Header.module.css';
+import {Modal} from 'antd';
 
 const Header: FC = () => {
+    const [signupModal, setSignupModal] = useState<boolean>(false);
+    const [loginModal, setLoginModal] = useState<boolean>(false);
+    const [isLogin, setIsLogin] = useState<boolean>(false);
+
+    const [login, setLogin] = useState<any>({});
+
+    const loginOpenHandler = () => {
+        setLoginModal(true);
+        setSignupModal(false)
+    }
+
+    const signupOpenHandler = () => {
+        setSignupModal(true)
+        setLoginModal(false)
+    }
+    
+    const loginHandler = () => {
+        if (login?.id === '190103056' && login?.password === '1234'){
+            setIsLogin(true);
+            setLoginModal(false)
+        }
+    }
+
     return (
         <div className={styles.header}>
+            <Modal open={signupModal} width={582} onCancel={() => setSignupModal(false)} footer={[]}>
+                <div className={styles.signup__wrapper}>
+                    <div className={styles.signup__logo}>
+                        <img src='logo.png'/>
+                    </div>
+                    <div className={styles.signup__title}>
+                        Sign Up
+                    </div>
+                    <div className={styles.signup__input}>
+                        <input placeholder='Student ID' />
+                    </div>
+                    <div className={styles.signup__input}>
+                        <input placeholder='Password' />
+                    </div>
+                    <div className={styles.signup__input}>
+                        <input placeholder='Firstname' />
+                    </div>
+                    <div className={styles.signup__input}>
+                        <input placeholder='Lastname' />
+                    </div>
+                    <div className={styles.signup__input}>
+                        <input placeholder='Email' />
+                    </div>
+                    <div className={styles.signup__input}>
+                        <input placeholder='Phone' />
+                    </div>
+                    <div className={styles.signup__input_wrapper}>
+                        <div className={styles.signup__input}>
+                            <input placeholder='Program' />
+                        </div>
+                        <div className={styles.signup__input}>
+                            <input placeholder='Course' />
+                        </div>
+                    </div>
+                    <div className={styles.signup__button}>
+                        Sign Up
+                    </div>
+                    <div className={styles.signup__link}>
+                        Already have an account? <span onClick={loginOpenHandler}>Login</span>
+                    </div>
+                </div>
+            </Modal>
+            <Modal open={loginModal} width={582} onCancel={() => setLoginModal(false)} footer={[]}>
+                <div className={styles.signup__wrapper}>
+                    <div className={styles.signup__logo}>
+                        <img src='logo.png'/>
+                    </div>
+                    <div className={styles.signup__title}>
+                        Login
+                    </div>
+                    <div className={styles.signup__input}>
+                        <input placeholder='ID' value={login?.id} onChange={(e) => setLogin({...login, id: e.target.value})}/>
+                    </div>
+                    <div className={styles.signup__input}>
+                        <input placeholder='Password' type='password' value={login?.password} onChange={(e) => setLogin({...login, password: e.target.value})} />
+                    </div>
+                    <div className={styles.login__link}>
+                        Forgotten your username or password?
+                    </div>
+                    <div className={styles.signup__button} onClick={loginHandler}>
+                        Login
+                    </div>
+                    <div className={styles.signup__link}>
+                        Already have an account? <span onClick={signupOpenHandler}>Sign Up</span>
+                    </div>
+                </div>
+            </Modal>    
             <div className='container'>
                 <div className={styles.header__wrapper}>
                     <div className={styles.header__wrapper_nav}>
@@ -33,12 +126,25 @@ const Header: FC = () => {
                                 <img src='arrow-down.svg' />
                             </div>
                         </div>
-                        <div className={styles.header__wrapper_signup}>
-                            Sign Up
-                        </div>
-                        <div className={styles.header__wrapper_login}>
-                            Log In
-                        </div>
+                        {
+                            !isLogin ? 
+                            <>
+                                <div className={styles.header__wrapper_signup} onClick={() => setSignupModal(true)}>
+                                    Sign Up
+                                </div>
+                                <div className={styles.header__wrapper_login} onClick={() => setLoginModal(true)}>
+                                    Log In
+                                </div>
+                            </>
+                            : 
+                            <>
+                                <div className={styles.icons}>
+                                    <img src='bell.svg' />
+                                    <img src='account.svg' />
+                                </div>
+                            </>
+                        }
+                       
                     </div>
                 </div>
             </div>
